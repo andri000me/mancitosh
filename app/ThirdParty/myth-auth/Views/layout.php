39120edaf963991dff1641540f5f46d1cdeff270
kampungsurga <54826368+kampungsurga@users.noreply.github.com>
2021-01-21 23:38:35 +0700

    change bs cdn

--- app/ThirdParty/myth-auth/Views/layout.php
+++ app/ThirdParty/myth-auth/Views/layout.php
@@ -10,7 +10,7 @@
     <title><?= config('Site')->title ?></title>
 
     <!-- Bootstrap core CSS -->
-	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+	<link rel="stylesheet" href="<?= base_url('assets/css/bootstrap.min.css') ?>">
     <style>
         body {
             padding-top: 5rem;
