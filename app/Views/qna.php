f5804a24f30e63f67e5b587359566589f9d4f581
a42sj <a43sj@users.noreply.github.com>
2021-01-30 10:43:02 +0700

    link zoom

--- app/Views/qna.php
+++ app/Views/qna.php
@@ -5,9 +5,10 @@ $this->section('content');
 ?>
 
 <div class="container text-center py-3 py-xl-5">
-	<a href="#zoom" class="btn btn-success btn-block btn-lg">
+	<a href="https://us02web.zoom.us/j/81794704908?pwd=cmlibXVDU2F5V1NERE44Mk9paVhNZz09" class="btn btn-success btn-block btn-lg mb-1" target="_blank">
 		<div class="font-weight-bold border-bottom mb-1 pb-2">Link Zoom Meeting</div>
-		<small>password: man1ponorogo</small>
+		<small class="d-block">Meeting ID: 817 9470 4908</small>
+		<small class="d-block">Passcode: man1</small>
 	</a>
 	<!-- <h1 class="display-4"><?= $title ?></h1> -->
 	<!-- <p class="lead"><?= $description ?></p> -->
