dd75c0d06a72b1ff4705cd8198546b0aafbedfd9
kampungsurga <x570+kampungsurga@users.noreply.github.com>
2021-02-01 20:27:30 +0700

    peserta/vlog
    WhatsApp text

--- app/Views/peserta/vlog.php
+++ app/Views/peserta/vlog.php
@@ -25,7 +25,7 @@ $this->section('content');
 						<td><?= $no ?></td>
 						<td><a href="<?= site_url( 'peserta/vlog/' . $peserta->id ) ?>"><?= $peserta->ketua ?></a></td>
 						<td><?= $peserta->sekolah ?></td>
-						<td><?= anchor( 'https://api.whatsapp.com/send/?phone=62' . $peserta->telepon . '&text=dari+panitia+mancitosh&app_absent=1', '0' . $peserta->telepon ) ?></td>
+						<td><?= anchor( 'https://api.whatsapp.com/send/?phone=62' . $peserta->telepon . '&text=Assalamualaikum%2C%20Selamat%20Pendaftaran%20anda%20sudah%20berhasil.%20Selanjutnya%20silahkan%20bergabung%20dengan%20grup%20pendaftar%20untuk%20tanya%20jawab%20dan%20mendapat%20informasi%20selanjutnya.%20Terimakasih.%0D%0Ahttps%3A%2F%2Fchat.whatsapp.com%2FH9HiziBr6Gi5jNrObsz8Ha&app_absent=1', '0' . $peserta->telepon ) ?></td>
 						<td>
 							<?php
 							$cek_berkas = $berkas->where('telepon', $peserta->telepon)->first();
